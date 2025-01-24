813eb3e3397d0164c3cffa53922789bf8e4c2f3f
bhanuprasad2004 <bhanuprasadadunuri@gmail.com>
2025-01-24 18:53:28 +0530

    twosum

---
+++ src/TwoSum.java
@@ -0,0 +1,19 @@
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr = {2,7,10,6,4,8};
+        int target = 9;
+        twosum(arr, target);
+    }
+    public static void twosum(int[] arr, int target){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i +1; j < arr.length;j++) {
+            if (arr[i] + arr[j] == target) {
+                System.out.println("Indices: " + i + ", " + j);
+                System.out.println("Elements are : " +arr[i] + "," +arr[j]+ " = "+ target);
+                    return;
+            }
+        }
+    }
+       System.out.println("Elements Not found"); 
+    }
+}
